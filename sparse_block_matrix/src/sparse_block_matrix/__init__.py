4f9d6121fb0c2273f2c26a9d0e3329062cda5590
HannesSommer <hannes.sommer@mavt.ethz.ch>
2014-05-05 15:02:17 +0200

    removed some load_manifest

--- sparse_block_matrix/src/sparse_block_matrix/__init__.py
+++ sparse_block_matrix/src/sparse_block_matrix/__init__.py
@@ -1,5 +1,5 @@
 # Import the numpy to Eigen type conversion.
-import roslib; roslib.load_manifest('numpy_eigen'); import numpy_eigen
+import numpy_eigen
 # Import the the C++ exports from your package library.
 from libsparse_block_matrix_python import *
 # Import other files in the directory
