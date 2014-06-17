ee13bf3a7674d9340ebcf94e71977714fd0b85e3
Mathias Bürki <mbuerki@mbuerki-W530.(none)>
2014-06-17 19:55:44 +0200

    added a pair for design variable pointer and a time variable

--- aslam_backend/include/aslam/backend/DesignVariable.hpp
+++ aslam_backend/include/aslam/backend/DesignVariable.hpp
@@ -2,6 +2,7 @@
 #define ASLAM_DESIGN_VARIABLE_HPP
 
 #include <sm/Id.hpp>
+#include <sm/timing/NsecTimeUtilities.hpp>
 #include <unordered_set>
 #include <set>
 
@@ -12,6 +13,13 @@
 
 namespace aslam {
   namespace backend {
+    class DesignVariable;
+
+    struct DesignVariableTimePair
+    {
+        DesignVariable* dv;
+        sm::timing::NsecTime t;
+    };
 
     class JacobianContainer;
 
