b7e2fdefe0415169a4c2261a51cb6109b7eac419
Ulrich Schwesinger <ulrich.schwesinger@mavt.ethz.ch>
2015-08-06 15:48:56 +0200

    Initial version of hamiltonian Markov chain monte carlo algorithm

--- aslam_backend/test/TestSamplerMcmc.cpp
+++ aslam_backend/test/TestSamplerMcmc.cpp
@@ -1,7 +1,7 @@
 #include <sm/eigen/gtest.hpp>
-#include <sm/timing/Timer.hpp>
 #include <sm/random.hpp>
 #include <aslam/backend/SamplerMcmc.hpp>
+#include <aslam/backend/SamplerHmc.hpp>
 #include <aslam/backend/OptimizationProblem.hpp>
 #include <aslam/backend/ErrorTerm.hpp>
 #include <aslam/backend/test/ErrorTermTester.hpp>
@@ -46,35 +46,43 @@ public:
 
 };
 
+boost::shared_ptr<OptimizationProblem> setupProblem(const double meanTrue, const double sigmaTrue) {
+
+  sm::random::seed(std::time(nullptr));
+
+  boost::shared_ptr<OptimizationProblem> gaussian1dLogDensityPtr(new OptimizationProblem);
+  OptimizationProblem& gaussian1dLogDensity = *gaussian1dLogDensityPtr;
+
+  Scalar::Vector1d x;
+  x << meanTrue + 10.0*sm::random::randn();
+
+  // Add some design variables.
+  boost::shared_ptr<Scalar> sdv(new Scalar(x));
+  gaussian1dLogDensity.addDesignVariable(sdv);
+  sdv->setBlockIndex(0);
+  sdv->setActive(true);
+  // Add error term
+  boost::shared_ptr<GaussianNegLogDensityError> err(new GaussianNegLogDensityError(sdv.get()));
+  err->setMean(meanTrue);
+  err->setVariance(sigmaTrue*sigmaTrue);
+  gaussian1dLogDensity.addErrorTerm(err);
+  SCOPED_TRACE("");
+  testErrorTerm(err);
+
+  return gaussian1dLogDensityPtr;
+}
+
 
 TEST(OptimizerSamplerMcmcTestSuite, testSamplerMcmc)
 {
   try {
-    typedef OptimizationProblem LogDensity;
-    typedef boost::shared_ptr<LogDensity> LogDensityPtr;
 
     sm::random::seed(std::time(nullptr));
 
-    LogDensityPtr gaussian1dLogDensityPtr(new LogDensity);
-    OptimizationProblem& gaussian1dLogDensity = *gaussian1dLogDensityPtr;
-
     const double meanTrue = 10.0;
     const double sigmaTrue = 2.0;
-    Scalar::Vector1d x;
-    x << meanTrue + 10.0*sm::random::randn();
-
-    // Add some design variables.
-    boost::shared_ptr<Scalar> sdv(new Scalar(x));
-    gaussian1dLogDensity.addDesignVariable(sdv);
-    sdv->setBlockIndex(0);
-    sdv->setActive(true);
-    // Add error term
-    boost::shared_ptr<GaussianNegLogDensityError> err(new GaussianNegLogDensityError(sdv.get()));
-    err->setMean(meanTrue);
-    err->setVariance(sigmaTrue*sigmaTrue);
-    gaussian1dLogDensity.addErrorTerm(err);
-    SCOPED_TRACE("");
-    testErrorTerm(err);
+    boost::shared_ptr<OptimizationProblem> gaussian1dLogDensityPtr = setupProblem(meanTrue, sigmaTrue);
+    OptimizationProblem& gaussian1dLogDensity = *gaussian1dLogDensityPtr;
 
     // Initialize and test options
     sm::BoostPropertyTree pt;
@@ -133,9 +141,98 @@ TEST(OptimizerSamplerMcmcTestSuite, testSamplerMcmc)
     EXPECT_DOUBLE_EQ(sampler.getAcceptanceRate(), 0.0);
     EXPECT_EQ(sampler.getNumIterations(), 0);
 
-#ifdef aslam_backend_ENABLE_TIMING
-    sm::timing::Timing::print(cout, sm::timing::SORT_BY_TOTAL);
-#endif
+  } catch (const std::exception& e) {
+    FAIL() << e.what();
+  }
+}
+
+
+#include <sm/logging.hpp>
+
+TEST(OptimizerSamplerMcmcTestSuite, testSamplerHmc)
+{
+  try {
+
+    sm::random::seed(std::time(nullptr));
+
+    const double meanTrue = 10.0;
+    const double sigmaTrue = 2.0;
+    boost::shared_ptr<OptimizationProblem> gaussian1dLogDensityPtr = setupProblem(meanTrue, sigmaTrue);
+    OptimizationProblem& gaussian1dLogDensity = *gaussian1dLogDensityPtr;
+
+    // Initialize and test options
+    sm::BoostPropertyTree pt;
+    pt.setDouble("delta", 0.1);
+    pt.setDouble("nHamiltonianSteps", 50);
+    pt.setDouble("nThreads", 1);
+    SamplerHmcOptions options(pt);
+    EXPECT_DOUBLE_EQ(pt.getDouble("delta"), options.delta);
+    EXPECT_DOUBLE_EQ(pt.getInt("nHamiltonianSteps"), options.nHamiltonianSteps);
+    EXPECT_DOUBLE_EQ(pt.getInt("nThreads"), options.nThreads);
+
+    // Set and test log density
+    SamplerHmc sampler(options);
+    sampler.setNegativeLogDensity(gaussian1dLogDensityPtr);
+    EXPECT_NO_THROW(sampler.checkNegativeLogDensitySetup());
+    EXPECT_DOUBLE_EQ(sampler.statistics().getAcceptanceRate(), 0.0);
+    EXPECT_EQ(sampler.statistics().getNumIterations(), 0);
+
+    // Parameters
+    const int nSamples = 1000;
+    const int nStepsBurnIn = 10;
+    const int nStepsSkip = 5;
+
+    // Burn-in
+    sampler.run(nStepsBurnIn);
+    EXPECT_GT(sampler.statistics().getAcceptanceRate(false), 0.0);
+    EXPECT_LE(sampler.statistics().getAcceptanceRate(false), 1.0);
+    EXPECT_GT(sampler.statistics().getAcceptanceRate(true), 0.0);
+    EXPECT_LE(sampler.statistics().getAcceptanceRate(true), 1.0);
+    EXPECT_EQ(sampler.statistics().getNumIterations(true), nStepsBurnIn);
+    EXPECT_EQ(sampler.statistics().getNumIterations(false), nStepsBurnIn);
+
+    // Now let's retrieve samples
+    Eigen::VectorXd dvValues(nSamples);
+    for (size_t i=0; i<nSamples; i++) {
+      sampler.run(nStepsSkip);
+      EXPECT_GE(sampler.statistics().getAcceptanceRate(), 0.0);
+      EXPECT_LE(sampler.statistics().getAcceptanceRate(), 1.0);
+      EXPECT_EQ(sampler.statistics().getNumIterations(false), nStepsSkip);
+      EXPECT_EQ(sampler.statistics().getNumIterations(true), (i+1)*nStepsSkip + nStepsBurnIn);
+      ASSERT_EQ(1, gaussian1dLogDensity.numDesignVariables());
+      auto dv = gaussian1dLogDensity.designVariable(0);
+      Eigen::MatrixXd p;
+      dv->getParameters(p);
+      ASSERT_EQ(1, p.size());
+      dvValues[i] = p(0,0);
+    }
+
+    EXPECT_DOUBLE_EQ((double)sampler.statistics().getNumAcceptedSamples(true)/(double)sampler.statistics().getNumIterations(true),
+                     sampler.statistics().getAcceptanceRate(true));
+
+    // check sample mean
+//    std::cout << "mean est: " << dvValues.mean() << ", mean true: " << meanTrue << std::endl;
+//    std::cout << "var est: " << (dvValues.array() - dvValues.mean()).matrix().squaredNorm()/(dvValues.rows() - 1.0) << ", var true: " << sigmaTrue*sigmaTrue << std::endl;
+
+    EXPECT_NEAR(dvValues.mean(), meanTrue, 4.*sigmaTrue) << "This failure does not necessarily have to be an error. It should just appear "
+        " with a probability of 0.00633 %";
+
+    // check sample variance
+    EXPECT_NEAR((dvValues.array() - dvValues.mean()).matrix().squaredNorm()/(dvValues.rows() - 1.0), sigmaTrue*sigmaTrue, 1e0) << "This failure does "
+        "not necessarily have to be an error. It should just appear very rarely";
+
+    // Run until a specified number of samples was accepted
+    sampler.run(numeric_limits<size_t>::max(), 1);
+    EXPECT_EQ(sampler.statistics().getNumAcceptedSamples(false), 1);
+    EXPECT_GT(sampler.statistics().getAcceptanceRate(false), 0.0);
+    EXPECT_GT(sampler.statistics().getNumIterations(false), 0);
+    EXPECT_GT(sampler.statistics().getAcceptanceRate(true), 0.0);
+    EXPECT_GT(sampler.statistics().getNumIterations(true), 0);
+
+    // Check that re-initializing resets values
+    sampler.initialize();
+    EXPECT_DOUBLE_EQ(sampler.statistics().getAcceptanceRate(), 0.0);
+    EXPECT_EQ(sampler.statistics().getNumIterations(), 0);
 
   } catch (const std::exception& e) {
     FAIL() << e.what();
