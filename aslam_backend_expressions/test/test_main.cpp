239ce79372d7f70b37d6aef8275a3036b700eb90
Ulrich Schwesinger <ulrich.schwesinger@mavt.ethz.ch>
2015-08-19 15:44:16 +0200

    Added random seed to randomize test points upon subsequent calls

--- aslam_backend_expressions/test/test_main.cpp
+++ aslam_backend_expressions/test/test_main.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 
+#include <sm/random.hpp>
+
 /// Run all the tests that were declared with TEST()
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
+  sm::random::seed(std::time(nullptr));
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
