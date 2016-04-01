bdc9f00ec1ab440ddc3e2d05badb98f71b658d3a
Ulrich Schwesinger <ulrich.schwesinger@mavt.ethz.ch>
2016-04-01 11:54:21 +0200

    Simplify ScalarExpression Jacobian computations

--- aslam_backend_expressions/include/aslam/backend/implementation/ScalarExpressionNode.hpp
+++ aslam_backend_expressions/include/aslam/backend/implementation/ScalarExpressionNode.hpp
@@ -42,12 +42,8 @@ double ScalarExpressionNodeMultiply::evaluateImplementation() const
 
 void ScalarExpressionNodeMultiply::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> L(1,1), R(1,1);
-  L(0,0) = _lhs->toScalar();
-  R(0,0) = _rhs->toScalar();
-
-  _lhs->evaluateJacobians(outJacobians.apply(R));
-  _rhs->evaluateJacobians(outJacobians.apply(L));
+  _lhs->evaluateJacobians(outJacobians.apply(_rhs->toScalar()));
+  _rhs->evaluateJacobians(outJacobians.apply(_lhs->toScalar()));
 }
 
 void ScalarExpressionNodeMultiply::evaluateJacobiansImplementation(JacobianContainer & outJacobians, const Eigen::MatrixXd & applyChainRule) const
@@ -62,11 +58,9 @@ double ScalarExpressionNodeDivide::evaluateImplementation() const
 
 void ScalarExpressionNodeDivide::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> L(1,1), R(1,1);
   const auto rhs_rec = 1./_rhs->toScalar();
-  L(0,0) = rhs_rec;
-  R(0,0) = -_lhs->toScalar() * rhs_rec * rhs_rec;
-  _lhs->evaluateJacobians(outJacobians.apply(L));
+  const auto R = -_lhs->toScalar() * rhs_rec * rhs_rec;
+  _lhs->evaluateJacobians(outJacobians.apply(rhs_rec));
   _rhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -84,9 +78,7 @@ double ScalarExpressionNodeNegated::evaluateImplementation() const
 
 void ScalarExpressionNodeNegated::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  R(0,0) = -1;
-  _rhs->evaluateJacobians(outJacobians.apply(R));
+  _rhs->evaluateJacobians(outJacobians.apply(-1.0));
 }
 
 void ScalarExpressionNodeNegated::evaluateJacobiansImplementation(JacobianContainer & outJacobians, const Eigen::MatrixXd & applyChainRule) const
@@ -102,10 +94,8 @@ double ScalarExpressionNodeAdd::evaluateImplementation() const
 
 void ScalarExpressionNodeAdd::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  R(0,0) = _multiplyRhs;
   _lhs->evaluateJacobians(outJacobians);
-  _rhs->evaluateJacobians(outJacobians.apply(R));
+  _rhs->evaluateJacobians(outJacobians.apply(_multiplyRhs));
 }
 
 void ScalarExpressionNodeAdd::evaluateJacobiansImplementation(JacobianContainer & outJacobians, const Eigen::MatrixXd & applyChainRule) const
@@ -125,8 +115,7 @@ double ScalarExpressionNodeSqrt::evaluateImplementation() const
 void ScalarExpressionNodeSqrt::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
   using std::sqrt;
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  R(0,0) = 1./(2.*sqrt(_lhs->toScalar()));
+  const auto R = 1./(2.*sqrt(_lhs->toScalar()));
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -146,8 +135,7 @@ double ScalarExpressionNodeLog::evaluateImplementation() const
 
 void ScalarExpressionNodeLog::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  R(0,0) = 1./(_lhs->toScalar());
+  const auto R = 1./(_lhs->toScalar());
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -166,8 +154,7 @@ double ScalarExpressionNodeExp::evaluateImplementation() const
 void ScalarExpressionNodeExp::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
   using std::exp;
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  R(0,0) = exp(_lhs->toScalar());
+  const auto R = exp(_lhs->toScalar());
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -185,9 +172,8 @@ double ScalarExpressionNodeAtan::evaluateImplementation() const
 
 void ScalarExpressionNodeAtan::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
-  Eigen::Matrix<double, 1, 1> R(1,1);
   const auto lhss = _lhs->toScalar();
-  R(0,0) = 1./(1. + lhss * lhss);
+  const auto R = 1./(1. + lhss * lhss);
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -206,13 +192,11 @@ double ScalarExpressionNodeAtan2::evaluateImplementation() const
 void ScalarExpressionNodeAtan2::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
   // _rhs corresponds to x, _lhs to y
-  Eigen::Matrix<double, 1, 1> R(1,1);
-  Eigen::Matrix<double, 1, 1> L(1,1);
   const auto lhs = _lhs->toScalar();
   const auto rhs = _rhs->toScalar();
   const double factor = 1./(lhs*lhs + rhs*rhs);
-  R(0,0) = -lhs*factor;
-  L(0,0) = rhs*factor;
+  const auto R = -lhs*factor;
+  const auto L = rhs*factor;
   _lhs->evaluateJacobians(outJacobians.apply(L));
   _rhs->evaluateJacobians(outJacobians.apply(R));
 }
@@ -227,18 +211,16 @@ double ScalarExpressionNodeAcos::evaluateImplementation() const
 {
   using std::acos;
   auto lhss = _lhs->toScalar();
-  SM_ASSERT_LE(Exception, lhss, 1.0, "");
-  SM_ASSERT_GE(Exception, lhss, -1.0, "");
+  SM_ASSERT_GE_LE(Exception, lhss, -1.0, 1.0, "");
   return acos(lhss);
 }
 
 void ScalarExpressionNodeAcos::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
   using std::sqrt;
-  Eigen::Matrix<double, 1, 1> R(1,1);
   const auto lhss = _lhs->toScalar();
   SM_ASSERT_LE(std::runtime_error, lhss, 1.0, "");
-  R(0,0) = -1./sqrt(1. - lhss*lhss);
+  const auto R = -1./sqrt(1. - lhss*lhss);
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -262,20 +244,15 @@ void ScalarExpressionNodeAcosSquared::evaluateJacobiansImplementation(JacobianCo
   using std::sqrt;
   using std::acos;
 
-  Eigen::Matrix<double, 1, 1> R(1,1);
   const auto lhss = _lhs->toScalar();
   auto pow1 = lhss - 1.0;
   auto pow2 = pow1 * pow1;
 
-  if (pow2 < std::numeric_limits<double>::epsilon())   // series expansion at x = 1
-  {
-    R(0,0) = -2.0 + 2.0/3.0*pow1;
-  }
-  else
-  {
-    R(0,0) = -2.0*acos(lhss)/sqrt(1.0 - lhss*lhss);
-  }
-  SM_ASSERT_FALSE(Exception, std::isnan(R(0,0)), "");
+  const double R = pow2 < std::numeric_limits<double>::epsilon() ?
+      -2.0 + 2.0/3.0*pow1 : // series expansion at x = 1
+      -2.0*acos(lhss)/sqrt(1.0 - lhss*lhss);
+  SM_ASSERT_FALSE(Exception, std::isnan(R), "");
+
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -296,24 +273,24 @@ void ScalarExpressionNodeInverseSigmoid::evaluateJacobiansImplementation(Jacobia
 {
   using std::exp;
 
-  Eigen::Matrix<double, 1, 1> R(1,1);
   const auto lhss = _lhs->toScalar();
   const double threshold = 50;
 
+  double R;
   if (lhss > threshold) {
     // approximate with Taylor expansion since exponents become too big otherwise
     auto den = exp(_scale*_shift) + exp(_scale*threshold);
     auto denSq = den*den;
-    R(0,0) = - _height*_scale*exp(_scale*(threshold+_shift)) / denSq +
+    R = - _height*_scale*exp(_scale*(threshold+_shift)) / denSq +
         _height*_scale*_scale*(lhss-threshold)*exp(_scale*(_shift+threshold))
         *(exp(threshold*_scale) - exp(_scale*_shift)) / (denSq*den);
   }
   else {
     auto den = 1 + exp(_scale*(lhss-_shift));
-    R(0,0) = - _height*_scale*exp(_scale*(lhss-_shift)) / (den * den);
+    R = - _height*_scale*exp(_scale*(lhss-_shift)) / (den * den);
   }
 
-  SM_ASSERT_FALSE(Exception, std::isnan(R(0,0)), "");
+  SM_ASSERT_FALSE(Exception, std::isnan(R), "");
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
@@ -333,10 +310,9 @@ double ScalarExpressionNodePower::evaluateImplementation() const
 void ScalarExpressionNodePower::evaluateJacobiansImplementation(JacobianContainer & outJacobians) const
 {
   using std::pow;
-  Eigen::Matrix<double, 1, 1> R(1,1);
   const auto lhss = _lhs->toScalar();
-  R(0,0) = _power * pow(lhss, _power-1);
-  SM_ASSERT_FALSE(Exception, std::isnan(R(0,0)), "");
+  const auto R = _power * pow(lhss, _power-1);
+  SM_ASSERT_FALSE_DBG(Exception, std::isnan(R), "");
   _lhs->evaluateJacobians(outJacobians.apply(R));
 }
 
