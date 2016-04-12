ethz-asl/aslam_optimizer

commit b477825bdea82bc4cb600c8f772edbacad164d9d

Support empty translation and rotation in TransformationBasic

aslam_backend_expressions/src/TransformationBasic.cpp
@@ -8,20 +8,20 @@ namespace aslam {
     TransformationBasic::TransformationBasic(RotationExpression C_0_1, EuclideanExpression t_0_1_0) :
       _rotation(C_0_1.root()), _translation(t_0_1_0.root())
     {
-
     }
 
     TransformationBasic::~TransformationBasic()
     {
-      
     }
 
     Eigen::Matrix4d TransformationBasic::toTransformationMatrixImplementation()
     {
       Eigen::Matrix4d T;
       T.setIdentity();
-      T.topLeftCorner<3,3>() = _rotation->toRotationMatrix();
-      T.topRightCorner<3,1>() = _translation->toEuclidean();
+      if(_rotation)
+        T.topLeftCorner<3,3>() = _rotation->toRotationMatrix();
+      if(_translation)
+        T.topRightCorner<3,1>() = _translation->toEuclidean();
       return T;
     }
 
@@ -30,46 +30,54 @@ namespace aslam {
       //S(x) = [ 1    - r^\times S(theta) ]
       //       [ 0         S(theta)       ]
       // Eigen::Matrix3d C = _rotation->toRotationMatrix();
-      Eigen::Vector3d r = _translation->toEuclidean();
+      if(_rotation){
+        Eigen::Matrix<double,6,3> crRotation;
+        if(_translation){
+          crRotation.topLeftCorner<3,3>() = -sm::kinematics::crossMx(_translation->toEuclidean());
+        } else {
+          crRotation.topLeftCorner<3,3>().setZero();
+        }
+        _rotation->evaluateJacobians(outJacobians, crRotation);
+      }
 
-      Eigen::Matrix<double,6,3> crRotation;
-      crRotation.topLeftCorner<3,3>() = -sm::kinematics::crossMx(r);
-      crRotation.bottomLeftCorner<3,3>().setIdentity();
-      
-      _rotation->evaluateJacobians(outJacobians, crRotation);
-      
-      Eigen::Matrix<double,6,3> crTranslation;
-      crTranslation.topLeftCorner<3,3>().setIdentity();
-      crTranslation.bottomLeftCorner<3,3>().setZero();
-      _translation->evaluateJacobians(outJacobians, crTranslation);
-      
-
+      if(_translation) {
+        Eigen::Matrix<double,6,3> crTranslation;
+        crTranslation.topLeftCorner<3,3>().setIdentity();
+        crTranslation.bottomLeftCorner<3,3>().setZero();
+        _translation->evaluateJacobians(outJacobians, crTranslation);
+      }
     }
+
     void TransformationBasic::evaluateJacobiansImplementation(JacobianContainer & outJacobians, const Eigen::MatrixXd & applyChainRule) const
     {
-
       //S(x) = [ 1    - r^\times S(theta) ]
       //       [ 0         S(theta)       ]
       // Eigen::Matrix3d C = _rotation->toRotationMatrix();
-      Eigen::Vector3d r = _translation->toEuclidean();
-
-      Eigen::Matrix<double,6,3> crRotation;
-      crRotation.topLeftCorner<3,3>() = -sm::kinematics::crossMx(r);
-      crRotation.bottomLeftCorner<3,3>().setIdentity();
+      if(_rotation){
+        Eigen::Matrix<double,6,3> crRotation;
+        if(_translation){
+          crRotation.topLeftCorner<3,3>() = -sm::kinematics::crossMx(_translation->toEuclidean());
+        } else {
+          crRotation.topLeftCorner<3,3>().setZero();
+        }
+        crRotation.bottomLeftCorner<3,3>().setIdentity();
+        _rotation->evaluateJacobians(outJacobians, applyChainRule * crRotation);
+      }
       
-      _rotation->evaluateJacobians(outJacobians, applyChainRule * crRotation);
-      
-      Eigen::Matrix<double,6,3> crTranslation;
-      crTranslation.topLeftCorner<3,3>().setIdentity();
-      crTranslation.bottomLeftCorner<3,3>().setZero();
-      _translation->evaluateJacobians(outJacobians, applyChainRule * crTranslation);
-      
+      if(_translation) {
+        Eigen::Matrix<double,6,3> crTranslation;
+        crTranslation.topLeftCorner<3,3>().setIdentity();
+        crTranslation.bottomLeftCorner<3,3>().setZero();
+        _translation->evaluateJacobians(outJacobians, applyChainRule * crTranslation);
+      }
     }
 
     void TransformationBasic::getDesignVariablesImplementation(DesignVariable::set_t & designVariables) const
     {
-      _rotation->getDesignVariables(designVariables);
-      _translation->getDesignVariables(designVariables);
+      if(_rotation)
+        _rotation->getDesignVariables(designVariables);
+      if(_translation)
+        _translation->getDesignVariables(designVariables);
     }
 
 

aslam_backend_expressions/src/TransformationExpression.cpp
@@ -25,10 +25,10 @@ namespace aslam {
 
     }
 
-      TransformationExpression::TransformationExpression(const Eigen::Matrix4d & T)
-      {
-          _root.reset(new TransformationExpressionNodeConstant(T));
-      }
+    TransformationExpression::TransformationExpression(const Eigen::Matrix4d & T)
+    {
+      _root.reset(new TransformationExpressionNodeConstant(T));
+    }
 
     TransformationExpression::TransformationExpression(const RotationExpression & rotation, const EuclideanExpression & translation)
     {
@@ -38,65 +38,82 @@ namespace aslam {
 
     TransformationExpression::~TransformationExpression()
     {
-      
     }
 
     void TransformationExpression::getDesignVariables(DesignVariable::set_t & designVariables) const
     {
-      _root->getDesignVariables(designVariables);
+      if(_root)
+        _root->getDesignVariables(designVariables);
     }
 
 
     Eigen::Matrix4d TransformationExpression::toTransformationMatrix() const
     {
-      return _root->toTransformationMatrix();
+      if(_root){
+        return _root->toTransformationMatrix();
+      } else {
+        return Eigen::Matrix4d::Identity();
+      }
     }
   
   RotationExpression TransformationExpression::toRotationExpression() const {
-    boost::shared_ptr< RotationExpressionNode > een( new RotationExpressionNodeTransformation( _root ) );
-    return RotationExpression(een);
+    if(_root){
+      boost::shared_ptr< RotationExpressionNode > een( new RotationExpressionNodeTransformation( _root ) );
+      return RotationExpression(een);
+    } else {
+      return RotationExpression();
+    }
   }
   // HomogeneousExpression toHomogeneousExpression() const;
   EuclideanExpression TransformationExpression::toEuclideanExpression() const {
-    boost::shared_ptr< EuclideanExpressionNode > een( new EuclideanExpressionNodeTranslation( _root ) );
-    return EuclideanExpression(een);
+    if(_root){
+      boost::shared_ptr< EuclideanExpressionNode > een( new EuclideanExpressionNodeTranslation( _root ) );
+      return EuclideanExpression(een);
+    } else {
+      return EuclideanExpression();
+    }
   }
 
 
     void TransformationExpression::evaluateJacobians(JacobianContainer & outJacobians) const
     {
-      return _root->evaluateJacobians(outJacobians);
+      if(_root)
+        _root->evaluateJacobians(outJacobians);
     }
 
 
     void TransformationExpression::evaluateJacobians(JacobianContainer & outJacobians, const Eigen::MatrixXd & applyChainRule) const
     {
-      return _root->evaluateJacobians(outJacobians, applyChainRule);
+      if(_root)
+        _root->evaluateJacobians(outJacobians, applyChainRule);
     }
 
 
     EuclideanExpression TransformationExpression::operator*(const EuclideanExpression & rhs) const{
+      if(!_root) return rhs;
       return (*this * rhs.toHomogeneousExpression()).toEuclideanExpression();
     }
-
     HomogeneousExpression TransformationExpression::operator*(const HomogeneousExpression & rhs) const
     {
+      if(!_root) return rhs;
       boost::shared_ptr<HomogeneousExpressionNode> newRoot( new HomogeneousExpressionNodeMultiply(_root, rhs._root));
       return HomogeneousExpression(newRoot);
     }
     
     TransformationExpression TransformationExpression::operator*(const TransformationExpression & rhs) const
     {
+      if(!_root) return rhs;
+      if(!rhs._root) return *this;
       boost::shared_ptr<TransformationExpressionNode> newRoot( new TransformationExpressionNodeMultiply(_root, rhs._root));
       return TransformationExpression(newRoot);
     }
 
     TransformationExpression TransformationExpression::inverse() const
     {
+      if(!_root) return *this;
       boost::shared_ptr<TransformationExpressionNode> newRoot( new TransformationExpressionNodeInverse(_root));
       return TransformationExpression(newRoot);
     }
-  
 
   } // namespace backend
 } // namespace aslam

commit 6f72c9e8f22f2344cc14bc6645e45b7d10de7600

Fixed regression introduced with b477825bde

aslam_backend_expressions/src/TransformationBasic.cpp
@@ -37,6 +37,7 @@ namespace aslam {
         } else {
           crRotation.topLeftCorner<3,3>().setZero();
         }
+        crRotation.bottomLeftCorner<3,3>().setIdentity();
         _rotation->evaluateJacobians(outJacobians, crRotation);
       }
 