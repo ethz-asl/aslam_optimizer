a370befe8302361390b89ea23786e0a40e98ddd2
Ulrich Schwesinger <ulrich.schwesinger@mavt.ethz.ch>
2016-05-26 10:04:26 +0200

    Adapt Sgd unit test to linefit support

--- aslam_backend/test/TestOptimizerSgd.cpp
+++ aslam_backend/test/TestOptimizerSgd.cpp
@@ -2,79 +2,121 @@
 #include <algorithm>
 #include <ctime>        // std::time
 #include <cstdlib>      // std::rand, std::srand
+#include <initializer_list>
+
+#include <sm/random.hpp>
+#include <sm/BoostPropertyTree.hpp>
+
 #include <aslam/backend/OptimizerSgd.hpp>
 #include <aslam/backend/OptimizationProblem.hpp>
 #include <aslam/backend/ErrorTerm.hpp>
-#include <boost/ptr_container/ptr_vector.hpp>
-#include <sm/random.hpp>
 #include <aslam/backend/test/ErrorTermTester.hpp>
+
 #include "SampleDvAndError.hpp"
+#include "TestOptimizerSupport.hpp"
+
+TEST(OptimizerStochasticGradientDescentTestSuite, testLearningRateScheduleConstant)
+{
+  try {
+    using namespace aslam::backend;
+    {
+      LearningRateScheduleConstant schedule(2.0);
+      EXPECT_DOUBLE_EQ(2.0, schedule(0));
+      EXPECT_DOUBLE_EQ(2.0, schedule(1));
+    }
+    {
+      sm::BoostPropertyTree pt;
+      pt.setDouble("initial", -2.0);
+      EXPECT_ANY_THROW(LearningRateScheduleConstant schedule(pt));
+      pt.setDouble("initial", 2.0);
+      LearningRateScheduleConstant schedule(pt);
+      EXPECT_DOUBLE_EQ(pt.getDouble("initial"), schedule.initialRate());
+    }
+  } catch (const std::exception& e) {
+    FAIL() << e.what();
+  }
+}
+
+TEST(OptimizerStochasticGradientDescentTestSuite, testLearningRateScheduleOptimal)
+{
+  try {
+    using namespace aslam::backend;
+    {
+      LearningRateScheduleOptimal schedule(2.0, 0.5);
+      EXPECT_DOUBLE_EQ(2.0, schedule(0));
+      EXPECT_DOUBLE_EQ(2.0/(1.0 + 2.0*1/0.5), schedule(1));
+    }
+    {
+      sm::BoostPropertyTree pt;
+      pt.setDouble("initial", 2.0);
+      pt.setDouble("tau", -2.0);
+      EXPECT_ANY_THROW(LearningRateScheduleOptimal schedule(pt));
+      pt.setDouble("tau", 2.0);
+      LearningRateScheduleOptimal schedule(pt);
+      EXPECT_DOUBLE_EQ(pt.getDouble("tau"), schedule.tau());
+    }
+  } catch (const std::exception& e) {
+    FAIL() << e.what();
+  }
+}
+
+TEST(OptimizerStochasticGradientDescentTestSuite, testOptions)
+{
+  try {
+    using namespace aslam::backend;
+    OptimizerSgd::Options options;
+    TEST_INVALID_OPTION(options, learningRateSchedule, NULL);
+  } catch (const std::exception& e) {
+    FAIL() << e.what();
+  }
+}
 
 TEST(OptimizerStochasticGradientDescentTestSuite, testOptimizerStochasticGradientDescent)
 {
   try {
 
+    using namespace aslam::backend;
+
     sm::random::seed(std::time(NULL));
     std::srand ( unsigned ( std::time(NULL) ) ); // for random_shuffle
 
-    using namespace aslam::backend;
-
-    auto normal_dist = [&] (int) { return sm::random::randn(); };
-
-    // implement least squares line fit through data
-    // Set up problem representing a linear function fit of the form y = a + b*x + N(0,1)
-    // to noisy samples from a linear function
-    const double a = 1.0;
-    const double b = 2.0;
-    const Eigen::Vector2d paramTrue(a, b);
-    const double stddev = 0.1;
-    const int numErrors = 1000;
-
-    Point2d dv( paramTrue + 10.0*Eigen::Vector2d::NullaryExpr(normal_dist) ); // random initialization of design variable
-    dv.setBlockIndex(0);
-    dv.setActive(true);
-
+    const Eigen::Vector2d paramTrue(1.0, 2.0);
     std::vector< boost::shared_ptr<TestNonSquaredError> > ets;
-    ets.reserve(numErrors);
-    for (std::size_t e = 0; e < numErrors; ++e) {
-      const double x = (double)e/numErrors;
-      const double y = a + b*x + stddev*sm::random::randn();
-      boost::shared_ptr<TestNonSquaredError> err(new TestNonSquaredError(&dv, x, y));
-      ets.push_back(err);
-    }
+    Point2d dv;
+    auto problem = test::createLinefitProblem(ets, dv, paramTrue, 1000, 0.1);
+    const double initialError = test::evaluateError(*problem);
 
     // Now let's optimize.
     OptimizerSgd::Options options;
-    options.lambda0 = 1.0;
-    options.tau = ets.size()/2.0;
+    options.useDenseJacobianContainer = true;
+    options.learningRateSchedule.reset(new LearningRateScheduleOptimal(1.0, ets.size()/2.0));
     OptimizerSgd optimizer(options);
+    EXPECT_EQ(0, optimizer.getStatus().numIterations);
 
-    OptimizerSgd::Status ret;
     std::size_t cnt = 0;
-    for (size_t i=0; i<50; i++) {
-      std::vector< boost::shared_ptr<TestNonSquaredError> > shuffledErrorTerms = ets;
-      std::random_shuffle(shuffledErrorTerms.begin(), shuffledErrorTerms.end());
-      for (auto& et : shuffledErrorTerms ) {
-        SCOPED_TRACE("");
-        optimizer.addBatch( std::vector<ScalarNonSquaredErrorTerm*>(1, et.get()) );
+    const std::size_t numPasses = 1;
+    for (size_t i=0; i<numPasses; i++) {
+      std::random_shuffle(ets.begin(), ets.end());
+      for (auto& et : ets) {
+        SCOPED_TRACE("Adding batch");
+        optimizer.addBatch< std::initializer_list<boost::shared_ptr<TestNonSquaredError> > >( { et } );
+        SCOPED_TRACE("Optimizing");
         optimizer.optimize();
-        ret = optimizer.getStatus();
-        SM_VERBOSE_STREAM("f(x) = " << dv._v[0] << " + " << dv._v[1] << " * x");
-        cnt++;
-        EXPECT_EQ(cnt, ret.numIterations);
+        SM_VERBOSE_STREAM("f(x) = " << dv._v[0] << " + " << dv._v[1] << " * x, error = " << test::evaluateError(*problem));
+        EXPECT_EQ(++cnt, optimizer.getStatus().numIterations);
       }
     }
 
     optimizer.incrementNumberOfIterations(2);
-    EXPECT_EQ(50*ets.size()+2, ret.numIterations);
+    EXPECT_EQ(cnt+2, optimizer.getStatus().numIterations);
 
-    EXPECT_LT(ret.learningRate, options.lambda0);
+    const auto ret = optimizer.getStatus();
+    EXPECT_LT(ret.learningRate, options.learningRateSchedule->initialRate());
     EXPECT_GT(ret.learningRate, 0.0);
+    EXPECT_EQ(numPasses*ets.size(), ret.numDerivativeEvaluations);
     EXPECT_GE(ret.maxDeltaX, 0.0);
-    EXPECT_EQ(50*ets.size(), ret.numDerivativeEvaluations);
-
-    EXPECT_NEAR(dv._v[0], a, 1e-1);
-    EXPECT_NEAR(dv._v[1], b, 1e-1);
+    EXPECT_LT(test::evaluateError(*problem), initialError);
+    sm::eigen::assertNear(dv._v, paramTrue, 1e-1, SM_SOURCE_FILE_POS, "");
 
   } catch (const std::exception& e) {
     FAIL() << e.what();
