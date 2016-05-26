a4c25b4f25fbaea6a42086ef308e2fb9e67f60ab
Ulrich Schwesinger <ulrich.schwesinger@mavt.ethz.ch>
2016-05-26 10:04:46 +0200

    Adapt BFGS and RProp to linefit support

--- aslam_backend/test/TestOptimizerRprop.cpp
+++ aslam_backend/test/TestOptimizerRprop.cpp
@@ -1,60 +1,52 @@
+#include <ctime>
 #include <sm/eigen/gtest.hpp>
+#include <sm/random.hpp>
 #include <aslam/backend/OptimizerRprop.hpp>
 #include <aslam/backend/OptimizationProblem.hpp>
 #include <aslam/backend/ErrorTerm.hpp>
-#include <boost/ptr_container/ptr_vector.hpp>
-#include <sm/random.hpp>
 #include <aslam/backend/test/ErrorTermTester.hpp>
 #include "SampleDvAndError.hpp"
+#include "TestOptimizerSupport.hpp"
+
+
+TEST(OptimizerRpropTestSuite, testOptions)
+{
+  try {
+    using namespace aslam::backend;
+    OptimizerRprop::Options options;
+    TEST_INVALID_OPTION(options, etaMinus, 0.0);
+    TEST_INVALID_OPTION(options, etaPlus, 0.0);
+    TEST_INVALID_OPTION(options, initialDelta, 0.0);
+    TEST_INVALID_OPTION(options, minDelta, 0.0);
+    TEST_INVALID_OPTION(options, maxDelta, 0.0);
+  } catch (const std::exception& e) {
+    FAIL() << e.what();
+  }
+}
 
 TEST(OptimizerRpropTestSuite, testRpropNonSquaredErrorTerms)
 {
   try {
     using namespace aslam::backend;
-    boost::shared_ptr<OptimizationProblem> problem_ptr(new OptimizationProblem);
-    OptimizationProblem& problem = *problem_ptr;
-
     sm::random::seed( std::time(NULL) );
-    auto normal_dist = [&] (int) { return sm::random::randn(); };
 
-    // implement least squares line fit through data
-    // Set up problem representing a linear function fit of the form y = a + b*x + N(0,1)
-    // to noisy samples from a linear function
-    const double a = 1.0;
-    const double b = 2.0;
-    const Eigen::Vector2d paramTrue(a, b);
-    const double stddev = 0.1;
-    const int numErrors = 1000;
-
-    Point2d dv( paramTrue + 2.0*Eigen::Vector2d::NullaryExpr(normal_dist) ); // random initialization of design variable
-    auto p0 = dv._v;
-    dv.setBlockIndex(0);
-    dv.setColumnBase(0);
-    dv.setActive(true);
-    problem.addDesignVariable(&dv, false);
-
+    // Create linefit problem
+    const Eigen::Vector2d paramTrue(1.0, 2.0);
     std::vector< boost::shared_ptr<TestNonSquaredError> > ets;
-    ets.reserve(numErrors);
-    for (std::size_t e = 0; e < numErrors; ++e) {
-      const double x = (double)e/numErrors;
-      const double y = a + b*x + stddev*sm::random::randn();
-      boost::shared_ptr<TestNonSquaredError> err(new TestNonSquaredError(&dv, x, y));
-      ets.push_back(err);
-      problem.addErrorTerm(err);
-    }
+    Point2d dv;
+    auto problem = test::createLinefitProblem(ets, dv, paramTrue, 1000, 0.1, 2.0);
+    auto p0 = dv._v;
+    const double initialError = test::evaluateError(*problem);
 
     // Now let's optimize.
     OptimizerRprop::Options options;
     options.maxIterations = 500;
     options.numThreadsGradient = 8;
     options.convergenceDeltaObjective = 0.0;
-    options.convergenceGradientNorm = 0.0;
     options.convergenceDeltaX = 0.0;
-    EXPECT_ANY_THROW(options.check());
     options.convergenceGradientNorm = 1e-6;
-    EXPECT_NO_THROW(options.check());
     OptimizerRprop optimizer(options);
-    optimizer.setProblem(problem_ptr);
+    optimizer.setProblem(problem);
 
     EXPECT_NO_THROW(optimizer.checkProblemSetup());
 
@@ -68,13 +60,13 @@ TEST(OptimizerRpropTestSuite, testRpropNonSquaredErrorTerms)
       optimizer.optimize();
       auto ret = optimizer.getStatus();
       EXPECT_TRUE(ret.success());
-      EXPECT_LE(ret.gradientNorm, 1e-6);
+      EXPECT_LE(ret.gradientNorm, options.convergenceGradientNorm);
       EXPECT_GE(ret.error, 0.0);
+      EXPECT_LT(test::evaluateError(*problem), initialError);
       EXPECT_LT(ret.maxDeltaX, 1e-3);
       if (method == OptimizerRprop::Options::IRPROP_PLUS)
         EXPECT_LT(ret.deltaError, 1e-12);
-      EXPECT_NEAR(dv._v[0], a, 1e-1);
-      EXPECT_NEAR(dv._v[1], b, 1e-1);
+      sm::eigen::assertNear(dv._v, paramTrue, 1e-1, SM_SOURCE_FILE_POS, "");
     }
 
   } catch (const std::exception& e) {
